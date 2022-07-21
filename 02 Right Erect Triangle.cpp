b8e8dc57b8e7642ea0361bfe01e39ba26d7b4787
hony18 <89496552+hony18@users.noreply.github.com>
2022-07-21 12:04:33 +0530

    Update 02 Right Erect Triangle.cpp

--- 02 Right Erect Triangle.cpp
+++ 02 Right Erect Triangle.cpp
@@ -8,7 +8,7 @@ int main() {
     cin >> n;
     for(int i=1; i<=m; i++){
         for(int j=1; j<=n; j++){
-            if(j > 5-i){
+            if(j > m-i){
                 cout << "*";
             }
             else{
