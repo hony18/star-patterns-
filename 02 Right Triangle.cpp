16d900069787bd6ac1d898a2f3f292b1310aaeaa
hony18 <89496552+hony18@users.noreply.github.com>
2022-07-21 11:52:18 +0530

    Create 02 Right Triangle.cpp

---
+++ 02 Right Triangle.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int m, n;
+    cin >> m;
+    cin >> n;
+    for(int i=1; i<=m; i++){
+        for(int j=1; j<=n; j++){
+            if(j > 5-i){
+                cout << "*";
+            }
+            else{
+                cout << " ";
+            }
+        }
+
+        cout << endl;
+    }
+	return 0;
+}
